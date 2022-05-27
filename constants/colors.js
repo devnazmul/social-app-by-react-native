7a47af459abc8e04bb930cf5d5b23c03a7ba97c8
Md Nazmul Islam <md.nazmul.islam1332@gmail.com>
2022-05-27 22:20:42 +0600

    create color constant

---
+++ constants/colors.js
@@ -0,0 +1,14 @@
+const COLORS = {
+    primaryDark:'#141414',
+    secondaryDark:'#333',
+
+    secondaryYellow:'#FECE00',
+
+    lightGray:'#5D5D5E',
+    darkGray:'#444',
+    
+    inputBackground:'#646464',
+    
+    white:'#ffffff',
+}
+export default COLORS;
