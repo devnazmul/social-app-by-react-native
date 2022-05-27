61d1f85ffa61e07ebb55f0bea4ea8abd87f14d14
Md Nazmul Islam <md.nazmul.islam1332@gmail.com>
2022-05-27 22:21:11 +0600

    Create Home.js

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,90 @@
+import { ScrollView, SafeAreaView, StyleSheet } from "react-native";
+import React from "react";
+import PostCard from "../../components/PostCard";
+
+import colors from "../../../constants/colors";
+
+const Home = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+    width: "100%",
+    height: "100%",
+    backgroundColor: colors.primaryDark,
+  },
+});
