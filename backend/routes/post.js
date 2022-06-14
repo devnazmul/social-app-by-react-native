cb005499665e31795f14702c9c63c728d1c96fd0
Md Nazmul Islam <md.nazmul.islam1332@gmail.com>
2022-06-14 19:23:38 +0600

    create post routes

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,5 +1,11 @@
-const Router = require('express').Router;
+const express   = require('express');
+const controller = require('../controllers').post;
 
-Router.get('/',()=>{
-    console.log('Get post');
-})
+const router = express.Router();
+
+router.post('/create', controller.createPost)
+// router.get('/', controller.test)
+// router.put('/update', controller.test)
+// router.delete('/delete', controller.test)
+
+module.exports = router;
