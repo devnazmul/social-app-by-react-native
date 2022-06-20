2340944c17f5ee09a3a4d6db5d43cbdd641131e1
Md Nazmul Islam <md.nazmul.islam1332@gmail.com>
2022-06-20 18:21:07 +0600

    create post controllers -> addComment ,getPost

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,9 +1,8 @@
-const { Post } = require("../models");
+const { Post, Comment } = require("../models");
 
 const createPost = (req, res) => {
     Post.create({
         uid: req.body.uid,
-        title: req.body.title,
         image: req.body.image,
         content: req.body.content,
     }).then((post) => {
@@ -13,7 +12,26 @@ const createPost = (req, res) => {
         res.status(500).send('Error: ' + error);
     })
 }
+const addComment = (req, res) => {
+    Comment.create({
+        uid: req.body.uid,
+        content: req.body?.content || '',
+        image: req.body?.image || '',
+    }).then((comment) => {
+        res.status(201).json(comment)
+    }).catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+    })
+}
 
+const getPost = (req,res) =>{
+    Post.find({},(err,posts)=>{
+        res.send(posts)
+    })
+}
 module.exports = {
     createPost,
+    addComment,
+    getPost,
 }
