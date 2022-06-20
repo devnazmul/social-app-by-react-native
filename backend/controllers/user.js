33f75686c23eef9df8bdf7bad6c76ad3297d28f3
Md Nazmul Islam <md.nazmul.islam1332@gmail.com>
2022-06-20 18:22:47 +0600

    create user crontrollers -> getSingleUser , createUser , loginUser , updateUser

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,24 @@
+const { hashSync, compareSync } = require("bcrypt");
 const { User } = require("../models");
+const jwt = require('jsonwebtoken');
 
-const createUser = (req, res) => {
+const getSingleUser = (req, res) => {
+    User.findOne({
+        userId: req.body.user_id,
+    }, (err, user) => {
+        err ? res.send({ err }) : res.status(200).send({
+            success: true,
+            data: {
+                username: user.fullName,
+                profilePic: {
+                    url: user.profilePicture
+                }
+            }
+        });
+    })
+}
+
+const createUser = (req, res, next) => {
     User.findOne({
         email: req.body.email,
     }, (err, user) => {
@@ -9,40 +27,79 @@ const createUser = (req, res) => {
             User.create({
                 fullName: req.body.fullName,
                 email: req.body.email,
-                password: req.body.password,
+                password: hashSync(req.body.password, 10),
             }).then((user) => {
-                res.send(user);
-            }).catch((err) => {
-                res.send(err)
+                res.send({
+                    success: true,
+                    message: 'user created successfully!',
+                    data: {
+                        userId: user._id,
+                        username: user.fullName,
+                        email: user.email,
+                    }
+                });
+            }).catch((error) => {
+                res.send({
+                    success: false,
+                    message: 'user not created! something was wrong!',
+                    error
+                })
             })
         } else {
             res.send({
+                success: false,
                 message: "user already exist with this email!"
             })
         }
     })
 }
 
-const loginUser = (req, res) => {
+const loginUser = (req, res, next) => {
     User.findOne({
         email: req.body.email,
     }).then((user) => {
-        if (user.password === req.body.password) {
-            res.send(user)
-        } else {
-            res.send({
-                message: 'invalid password or email!'
+        if (!user) {
+            res.status(401).send({
+                success: false,
+                message: 'userr not exist!.',
             })
+        } else {
+            if (compareSync(req.body.password, user.password)) {
+                payload = {
+                    userId: user._id,
+                    username: user.fullName,
+                    email: user.email,
+                }
+                const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: '7d' })
+                res.send({
+                    success: true,
+                    message: 'user loged in successfully.',
+                    data: {
+                        userId: user._id,
+                        username: user.fullName,
+                        email: user.email,
+                        profilePic: {
+                            url: user.profilePicture
+                        },
+                        token: `Bearer ${token}`
+                    }
+                })
+            } else {
+                res.send({
+                    success: false,
+                    message: 'invalid password or email!'
+                })
+            }
         }
-    }).catch((err) => {
+
+    }).catch((error) => {
         res.send({
-            err,
-            message: 'user not exist!'
+            success: false,
+            message: 'user err not exist!',
+            error: error
         })
     })
 }
-
-
 
 const updateUser = (req, res) => {
     User.findOneAndUpdate({
@@ -50,6 +107,7 @@ const updateUser = (req, res) => {
     }, req.body, { upsert: true }, (err, doc) => {
         if (err) return res.send(500, { error: err });
         return res.send({
+            success: true,
             message: 'succesfully saved.'
         });
     });
@@ -61,17 +119,29 @@ const deleteUser = (req, res) => {
         email: req.params.email
     }).then((user) => {
         if (!user) {
-            res.status(400).send(req.params.email + ' was not found');
+            res.status(400).send({
+                success: false,
+                message: req.params.email + ' was not found'
+            });
         } else {
-            res.status(200).send(req.params.email + ' was deleted.');
+            res.status(200).send({
+                success: true,
+                message: req.params.email + ' was deleted.'
+            });
         }
-    }).catch((err) => {
-        console.error(err);
-        res.status(500).send('Error: ' + err);
+    }).catch((error) => {
+        res.status(500).send(
+            {
+                success: false,
+                message: 'something was wrong!',
+                error
+            }
+        );
     });
 }
 
 module.exports = {
+    getSingleUser,
     createUser,
     loginUser,
     updateUser,
