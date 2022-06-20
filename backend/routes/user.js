ad0284f5f88630c11b29fdebfa3442a495980dfc
Md Nazmul Islam <md.nazmul.islam1332@gmail.com>
2022-06-20 18:24:46 +0600

    create user routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const controller = require('../controllers').user;
 
 
-
 router.post('/create',controller.createUser);
 router.post('/login',controller.loginUser);
-router.put('/update/:email',controller.updateUser);
-router.delete('/delete/:email',controller.deleteUser);
+router.get('/single',controller.getSingleUser);
+router.put('/update',controller.updateUser);
+router.delete('/delete',controller.deleteUser);
 
 
 module.exports = router;
