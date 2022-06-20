3ed0eb5cf818ad6f9af7ab6936786b9e76ba07f3
Md Nazmul Islam <md.nazmul.islam1332@gmail.com>
2022-06-20 18:25:50 +0600

    initialize passport package

--- backend/index.js
+++ backend/index.js
@@ -4,14 +4,11 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require("cors");
 const morgan = require('morgan');
+const passport = require('passport');
 
 require('dotenv').config();
 
-
-
-
 const app = express();
-
 
 // MIDDLEWARES 
 app.use(morgan('tiny'))
@@ -20,9 +17,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors());
 app.use(express.json());
+app.use(passport.initialize())
 
 // IMPORT ROUTES 
 const routes = require('./routes');
+
 
 app.use('/api/v1/posts/', routes.post);
 app.use('/api/v1/users/', routes.user);
