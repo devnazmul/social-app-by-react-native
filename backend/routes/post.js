6c92a127f677c5f7e759e1648de64ba0686597a6
Md Nazmul Islam <md.nazmul.islam1332@gmail.com>
2022-06-20 18:24:05 +0600

    create post routes

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,11 +1,14 @@
 const express   = require('express');
+const passport = require('passport');
 const controller = require('../controllers').post;
+require('../middlewares/passport'); 
 
 const router = express.Router();
 
-router.post('/create', controller.createPost)
-// router.get('/', controller.test)
-// router.put('/update', controller.test)
-// router.delete('/delete', controller.test)
+router.get('/',passport.authenticate('jwt',{session:false}),controller.getPost);
+router.post('/create',passport.authenticate('jwt',{session:false}),controller.createPost);
+router.post('/add-comment',controller.addComment);
+// router.put('/update/:email',controller.updateUser);
+// router.delete('/delete/:email',controller.deleteUser);
 
 module.exports = router;
